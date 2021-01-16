c2aa76ee0b61d1d59f268913669b3ebacee7d5a8
wang.zhengxu <wang.zhengxu@qq.com>
2021-01-16 22:59:06 +0800

    add logger

--- ChangeTokenExample/Program.cs
+++ ChangeTokenExample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Primitives;
@@ -9,19 +10,33 @@ namespace ChangeTokenExample
     {
         static async Task Main(string[] args)
         {
-            //            Console.WriteLine("start monitoring dir...");
-            //            var phyFileProvider = new PhysicalFileProvider("d:\\test");
-            //            ChangeToken.OnChange(
-            //                () => phyFileProvider.Watch("*.*"),
-            //                () => Console.WriteLine("file changed!")
-            //            );
-            Console.WriteLine("start adding books...");
-            var store = new Bookstore();
-            for (int i = 1; i <= 5; i++)
+           await Task.Delay(3000);
+            var fileProvider = new PhysicalFileProvider("d:\\test");
+
+            //IChangeToken changeToken = fileProvider.Watch("test.txt");
+            //changeToken.RegisterChangeCallback(_ =>
+            //{
+            //    Logger.ToConsole("file changed!",ConsoleColor.DarkGreen);
+            //},"");
+            ChangeToken.OnChange(
+                    () => fileProvider.Watch("test.txt"),
+                    () => Logger.ToConsole("file changed!", ConsoleColor.DarkGreen)
+                );
+            for (int i = 0; i < 3; i++)
             {
-                store.AddBook(i.ToString(), $"书{i}");
-                await Task.Delay(1000);
+                Logger.ToConsole("add text to file", ConsoleColor.DarkGray);
+                await File.AppendAllTextAsync(@"d:\test\test.txt", DateTime.Now.ToString());
+                await Task.Delay(2000);
+
             }
+
+            //            Console.WriteLine("start adding books...");
+            //            var store = new Bookstore();
+            //            for (int i = 1; i <= 5; i++)
+            //            {
+            //                store.AddBook(i.ToString(), $"书{i}");
+            //                await Task.Delay(1000);
+            //            }
 
             Console.ReadKey();
         }
